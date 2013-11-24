f97869296f57de80c856f27bbc5714c20cc81de6
aaronmalone <aaronmalone@gmail.com>
2013-11-24 13:38:42 -0500

    Minor logging changes.

--- lapis-core/src/main/java/edu/osu/lapis/LapisLogging.java
+++ lapis-core/src/main/java/edu/osu/lapis/LapisLogging.java
@@ -2,6 +2,7 @@ package edu.osu.lapis;
 
 import java.util.Properties;
 
+//NOTE: probably only need this for MatlabLapis
 public class LapisLogging {
 	
 	final static Properties newProps = new Properties(System.getProperties());
@@ -10,8 +11,8 @@ public class LapisLogging {
 	static {
 		newProps.setProperty("org.restlet.engine.loggerFacadeClass","org.restlet.ext.slf4j.Slf4jLoggerFacade");
 		newProps.setProperty("org.slf4j.simpleLogger.logFile", "System.out");
+		newProps.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "trace");
 		newProps.setProperty("org.slf4j.simpleLogger.log.edu.osu.lapis", "trace");
-		newProps.setProperty("org.slf4j.simpleLogger.log.org.restlet", "warn");
 	}
 	
 	public static synchronized void init() {
