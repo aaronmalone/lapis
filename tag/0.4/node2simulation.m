874b687ac8b7946832bd663197f0b160aaf24792
bjyurkovich <bj.yurkovich@gmail.com>
2014-01-16 15:55:27 -0500

    Updated MATLAB examples

--- tag/0.4/node2simulation.m
+++ tag/0.4/node2simulation.m
@@ -14,8 +14,6 @@
 clear all
 clear classes
 clear java
-javaaddpath([pwd '\lapis-matlab-0.4-jar-with-dependencies.jar']);  %add the lapis jar file.  Future releases will include this in the LapisAPI class.
-
 
 %% set up LAPIS
 coordinatorAddress = 'http://127.0.0.1:7777';
