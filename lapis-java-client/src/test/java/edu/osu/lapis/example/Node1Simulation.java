6cc56d67cca94d54317fc9288b772909ddaccd46
aaronmalone <aaronmalone@gmail.com>
2014-02-13 14:14:23 -0500

    Cleaned up example "simulations"

--- lapis-java-client/src/test/java/edu/osu/lapis/example/Node1Simulation.java
+++ lapis-java-client/src/test/java/edu/osu/lapis/example/Node1Simulation.java
@@ -1,5 +1,8 @@
 package edu.osu.lapis.example;
 
+import static edu.osu.lapis.Flags.evaluateFlagValue;
+import static edu.osu.lapis.Flags.getFlag;
+import static edu.osu.lapis.Flags.setFlagTrue;
 import static edu.osu.lapis.example.ExampleConstants.NODE_1;
 import static edu.osu.lapis.example.ExampleConstants.NODE_1_URL;
 import static edu.osu.lapis.example.ExampleConstants.NODE_2;
@@ -14,47 +17,43 @@ public class Node1Simulation {
 		
 		System.out.println("Node 1 starting...");
 		
-		LapisApi lapisApi = new LapisApi(NODE_1, NODE_1_URL); //TODO BETTER CONSTRUCTOR
+		LapisApi lapisApi = new LapisApi(NODE_1, NODE_1_URL);
 		
 		//publish variables
-		double[] countArray = new double[]{1,2,3,4,5}; //TODO MAYBE ADD BETTER WAY OF INSTANTIATING NEW ARRAYS
-		lapisApi.publishReadOnly("countArray", countArray);
+		double[] countArray = lapisApi.initializeAndPublishReadOnlyDoubleArray("countArray", 1,2,3,4,5);
 		
-		double[] finishedCountingUpFlag = new double[]{ 0 }; 
+		double[] finishedCountingUpFlag = getFlag(); 
 		lapisApi.publishReadOnly("finishedCountingUpFlag", finishedCountingUpFlag);
 		
-		double[] node1HasFinishedSimulationFlag = new double[]{ 0 };
+		double[] node1HasFinishedSimulationFlag = getFlag();
 		lapisApi.publishReadOnly("node1HasFinishedSimulationFlag", node1HasFinishedSimulationFlag);
 		
-		double[] node2HasFinishedCountingFlag = new double[]{ 0 }; //TODO RENAME?
+		double[] node2HasFinishedCountingFlag = getFlag(); //set by Node 2
 		lapisApi.publish("node2HasFinishedCountingFlag", node2HasFinishedCountingFlag);
 		
-		while(true) { //TODO MAYBE THERE'S A BETTER WAY OF DOING THIS?
+		while(countArray[0] < 10) {
 			for (int i = 0; i < countArray.length; i++) {
 				countArray[i] = countArray[i]+1;
 			}
 			printArray("countArray", countArray);
 			Sleep.sleep(250);
-			if(countArray[0] >= 10) {
-				finishedCountingUpFlag[0] = 1; //TODO STATIC FOR SETTING FLAGS
-				break;
-			}
 		}
+		setFlagTrue(finishedCountingUpFlag);
 		
 		lapisApi.ready();
 		
 		System.out.println("Done with my counting.\nWaiting for Node 2 to finish...");
 		
-		while(node2HasFinishedCountingFlag[0] == 0) {
+		while(evaluateFlagValue(node2HasFinishedCountingFlag)) {
 			System.out.println("Waiting for Node 2 to set 'node2HasFinishedCountingFlag' (a published variable of this node)...");
 			Sleep.sleep(1500);
 		}
 		
 		System.out.println("Getting Node 2's count array...");
 		double[] node2CountArray = lapisApi.getArrayOfDouble(NODE_2, "countArray");
-		printArray("Node 2's count array", node2CountArray); //TODO I DON'T THINK VALUE IS CORRECT!
+		printArray("Node 2's count array", node2CountArray);
 		
-		node1HasFinishedSimulationFlag[0] = 1; //TODO IMPROVE
+		setFlagTrue(node1HasFinishedSimulationFlag);
 		
 		lapisApi.redact("finishFlag");
 		lapisApi.redact("node2HasFinishedCountingFlag");
--- lapis-java-client/src/test/java/edu/osu/lapis/example/Node2Simulation.java
+++ lapis-java-client/src/test/java/edu/osu/lapis/example/Node2Simulation.java
@@ -1,5 +1,7 @@
 package edu.osu.lapis.example;
 
+import static edu.osu.lapis.Flags.FLAG_VALUE_TRUE;
+import static edu.osu.lapis.Flags.evaluateFlagValue;
 import static edu.osu.lapis.example.ExampleConstants.NODE_1;
 import static edu.osu.lapis.example.ExampleConstants.NODE_1_URL;
 import static edu.osu.lapis.example.ExampleConstants.NODE_2;
@@ -16,23 +18,22 @@ public class Node2Simulation {
 		
 		System.out.println("Node 2 starting...");
 		
-		LapisApi lapisApi = new LapisApi(NODE_2, NODE_1_URL, NODE_2_URL); //TODO BETTER CONSTRUCTOR
+		LapisApi lapisApi = new LapisApi(NODE_2, NODE_1_URL, NODE_2_URL);
 		
-		//publish variables
-		double[] countArray = new double[5];
-		lapisApi.publishReadOnly("countArray", countArray);
+		//publish variable
+		double[] countArray = lapisApi.initializeAndPublishReadOnlyDoubleArray("countArray", new double[5]); 
 		
 		lapisApi.waitForReadyNode(NODE_1, 8000);
-		
-		while(true) {
+
+		//wait for Node 1 to finish its counting
+		double[] node1FinishFlag;
+		do {
 			System.out.println("Waiting for Node 1 to set its 'finishedCountingUpFlag' variable...");
-			double[] node1FinishFlag = lapisApi.getArrayOfDouble("Node1", "finishedCountingUpFlag");
-			if(node1FinishFlag[0] == 1) {
-				break;
-			}
+			node1FinishFlag = lapisApi.getArrayOfDouble("Node1", "finishedCountingUpFlag");
 			Sleep.sleep(750);
-		}
+		} while(evaluateFlagValue(node1FinishFlag));
 		
+		//retrieve countArray from Node 1
 		double[] node1CountArray = lapisApi.getArrayOfDouble(NODE_1, "countArray");
 		System.arraycopy(node1CountArray, 0, countArray, 0, countArray.length);
 		
@@ -43,16 +44,16 @@ public class Node2Simulation {
 			System.out.println("countArray"+ ": " + Arrays.toString(countArray));
 			Sleep.sleep(200);
 		}
-		lapisApi.set(NODE_1, "node2HasFinishedCountingFlag", new double[]{1});
+		lapisApi.set(NODE_1, "node2HasFinishedCountingFlag", FLAG_VALUE_TRUE);
 		System.out.println("Done with my counting.");
 		
 		while(true) {
 			double[] fin = lapisApi.getArrayOfDouble(NODE_1, "node1HasFinishedSimulationFlag");
-			if(fin[0] == 1) {
+			if(evaluateFlagValue(fin)) {
 				break;
 			} else {
 				System.out.println("Waiting for Node 1 to set its 'node1HasFinishedSimulationFlag'...");
-				Sleep.sleep(500);
+				Sleep.sleep(750);
 			}
 		}
 		
