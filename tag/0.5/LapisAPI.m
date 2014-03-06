04439cfe8c58c846b5927e5ffa3c1540d7af2b68
aaronmalone <aaronmalone@gmail.com>
2014-03-06 11:47:16 -0500

    Changes in 0.5 tag for LAPISMap support

--- tag/0.5/LapisAPI.m
+++ tag/0.5/LapisAPI.m
@@ -1,14 +1,14 @@
 classdef LapisAPI < handle
-%     LAPIS API object.  Responsible for connecting and maintaing a LAPIS
-%     network connection. Use this API to do SETs and GETs on a LAPIS
-%     network.  Depends on a LAPIS java JAR file.
-% EXAMPLE:   
-% coordinatorAddress = 'http://127.0.0.1:7777';
-% nodeName = 'Node1';
-% lap = LapisAPI(nodeName, coordinatorAddress); 
-
-
-
+    %     LAPIS API object.  Responsible for connecting and maintaing a LAPIS
+    %     network connection. Use this API to do SETs and GETs on a LAPIS
+    %     network.  Depends on a LAPIS java JAR file.
+    % EXAMPLE:
+    % coordinatorAddress = 'http://127.0.0.1:7777';
+    % nodeName = 'Node1';
+    % lap = LapisAPI(nodeName, coordinatorAddress);
+    
+    
+    
     properties
         
         dataTable;          %Datatable for local published variables
@@ -36,7 +36,7 @@ classdef LapisAPI < handle
             org.apache.log4j.helpers.LogLog.setInternalDebugging(0);
             
             eval('import edu.osu.lapis.MatlabLapis');
-           
+            
             if nargin == 2  %Model is the coordinator
                 obj.modelName = varargin{1};
                 obj.coordinatorAddress = varargin{2};
@@ -70,111 +70,163 @@ classdef LapisAPI < handle
         end
         
         function obj = publishInternal(obj, data, readOnly)
-            if ~isa(data, 'LAPISData')
-                error('Published datatype must be type "LAPISData"');
+            if ~isa(data, 'LAPISData') && ~isa(data, 'LAPISMap')
+                error('Published datatype must be type "LAPISData" or "LAPISMap"');
             end
             
+            %Set Lapis Reference
             data.setLapisReference(obj);
-            obj.dataTable(data.name) = data;
             
-            if(readOnly == 1)
-                obj.lapisJava.publishReadOnly(java.lang.String(data.name), data.data);
-            else
-                obj.lapisJava.publish(java.lang.String(data.name), data.data);
+            if isa(data, 'LAPISData')
+                obj.dataTable(data.name) = data;
+                if(readOnly == 1)
+                    obj.lapisJava.publishReadOnly(java.lang.String(data.name), data.data);
+                else
+                    obj.lapisJava.publish(java.lang.String(data.name), data.data);
+                end
+            elseif isa(data, 'LAPISMap')
+                
+                obj.dataTable(data.name) = data;
+                if(readOnly == 1)
+                    obj.lapisJava.publishNewReadOnlyMap(java.lang.String(data.name));
+                else
+                    obj.lapisJava.publishNewMap(java.lang.String(data.name));
+                end
+                
             end
+            
         end
         
         function obj = redact(obj, data)
             % un-publish a varible
             obj.lapisJava.redact(data.name)
         end
-                
-        function obj = setCachedValue(obj, varName, data)    
-%             Setter to put a value into the LAPIS cache
+        
+        function obj = setCachedValue(obj, varName, data)
+            %             Setter to put a value into the LAPIS cache
             obj.lapisJava.setCachedValue(varName, data);
         end
         
         function result = retrieveCachedValue(obj, varName)
-%             Getter method to get a value from the LAPIS cache
-           result = obj.lapisJava.retrieveCachedValue(varName); 
+            %             Getter method to get a value from the LAPIS cache
+            result = obj.lapisJava.retrieveCachedValue(varName);
         end
         
         function obj = delete(obj)
-           %Deletes the object.
+            %Deletes the object.
             obj.shutdown;
         end
         
         function obj = set(obj,modelName, varName, data)
-           %Sets a variable on another LAPIS node.  Args(modelName, variablename, data)
-           
-           if ~isa(data, 'double')
-              % somebody who knows MATLAB can clean this up
-              if ~isa(data, 'char')
-                 error('Setting types other than double and char are not currently supported'); 
-              end
-           end
-           
-           fullName = [varName  '@'  modelName];
-           obj.lapisJava.set(fullName, data);
-           
+            %Sets a variable on another LAPIS node.  Args(modelName, variablename, data)
+            
+            if ~isa(data, 'double')
+                % somebody who knows MATLAB can clean this up
+                if ~isa(data, 'char')
+                    if ~isa(data, 'struct')
+                        error('Setting types other than double, char, or struct are not currently supported');
+                    end
+                end
+            end
+            
+            fullName = [varName  '@'  modelName];
+            
+            
+            if isa(data, 'struct')               
+                map = java.util.HashMap();
+                fnames = fieldnames(data);
+                
+                for i = 1:length(fnames)
+                   map.put(fnames{i}, getfield(data, fnames{i}));
+                end
+                
+                obj.lapisJava.set(fullName, map);
+                
+            else
+                obj.lapisJava.set(fullName, data);
+            end
+            
+            
+            
+            
         end
         
         function result = get(obj, modelName, varName)
+            
             %Gets a variable on another LAPIS node.  Args(modelName, variablename, data)
             fullName = [varName  '@'  modelName];
-            try 
+            try
                 result = obj.lapisJava.get(java.lang.String(fullName));
-            catch e 
+            catch e
                 disp('There was an error getting the value.  Please try again.');
                 result = obj.lapisJava.get(java.lang.String(fullName));
+            end
+
+            if isa(result, 'java.util.HashMap')
+                mapResult = struct;
+                iter = result.keySet.iterator;
+
+                while iter.hasNext()
+                    key = iter.next;
+                    
+                    if isa(result.get(key),'java.util.ArrayList')
+                       mapResult = setfield(mapResult, key,  cell2mat(java.util.Vector(result.get(key)).toArray().cell));
+                    else 
+                       mapResult = setfield(mapResult, key,  result.get(key));
+                    end
+                    
+                    
+                end
+
+                result = mapResult;
             end
         end
         
         function obj = shutdown(obj)
             %Shuts down the LAPIS nework.  This step is required if clearing variables.
-           obj.lapisJava.shutdown();
+            obj.lapisJava.shutdown();
         end
         
         function obj = ready(obj)
-        %ready
-        %  declares that this LAPIS node is now ready
-        %
-        %  The functions ready, notReady, waitForReadyNode, and
-        %  waitForReadyNodeWithTimeout facilitate coordination among
-        %  multiple nodes in a LAPIS network. It is not necessary for a
-        %  node to declare that it is ready by using the ready function
-        %  unless other nodes on the same network are or will be waiting
-        %  for the node to become ready using either the waitForReadyNode
-        %  or the waitForReadyNodeWithTimeout functions.
+            %ready
+            %  declares that this LAPIS node is now ready
+            %
+            %  The functions ready, notReady, waitForReadyNode, and
+            %  waitForReadyNodeWithTimeout facilitate coordination among
+            %  multiple nodes in a LAPIS network. It is not necessary for a
+            %  node to declare that it is ready by using the ready function
+            %  unless other nodes on the same network are or will be waiting
+            %  for the node to become ready using either the waitForReadyNode
+            %  or the waitForReadyNodeWithTimeout functions.
             obj.lapisJava.ready();
         end
         
         function obj = notReady(obj)
-        %notReady
-        %  declares that this LAPIS node is not ready
-        %  
-        %  The functions ready, notReady, waitForReadyNode, and
-        %  waitForReadyNodeWithTimeout facilitate coordination among
-        %  multiple nodes in a LAPIS network.
+            %notReady
+            %  declares that this LAPIS node is not ready
+            %
+            %  The functions ready, notReady, waitForReadyNode, and
+            %  waitForReadyNodeWithTimeout facilitate coordination among
+            %  multiple nodes in a LAPIS network.
             obj.lapisJava.notReady();
         end
-       
+        
         function obj = waitForReadyNode(obj, nodeName)
-        %waitForReadyNode
-        %  waitForReadyNode(N) waits indefinitely for node N to become
-        %  'ready' and does not time out
+            %waitForReadyNode
+            %  waitForReadyNode(N) waits indefinitely for node N to become
+            %  'ready' and does not time out
             obj.lapisJava.waitForReadyNode(nodeName);
         end
         
         function obj = waitForReadyNodeWithTimeout(obj, nodeName, millisToWait)
-        %waitForReadyNodeWithTimeout
-        %  waitForReadyNodeWithTimeout(N, M) waits for node N to become
-        %  'ready' and times out with an exception after M milliseconds if
-        %  node N has not become ready
+            %waitForReadyNodeWithTimeout
+            %  waitForReadyNodeWithTimeout(N, M) waits for node N to become
+            %  'ready' and times out with an exception after M milliseconds if
+            %  node N has not become ready
             
             obj.lapisJava.waitForReadyNode(nodeName, millisToWait);
         end
         
     end
-
+    
 end
